64f08629951df4dd6fead6aec83d51462032db92
sharifulislm <shariful.islam.coder@gmail.com>
2022-03-30 22:23:04 +0300

    added soemtihng new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Header from './components/Header/Header';
+import HomePage from './components/Homepage/Homepage';
+import About from './components/About/About';
 
-function App() {
+
+
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+<Header> </Header>
+      
+ <Routes> 
+<Route path='/' element={<HomePage></HomePage>}>  </Route>
+<Route path='About' element={<About> </About>}></Route>
+
+
+
+ </Routes>
+
     </div>
   );
-}
+};
 
 export default App;
